4056ee2c54eb3f37ae104fb39920486ea29afaa8
Grzegorz Pijanski <grzegorz.pijanski@snowflake.com>
2024-01-24 14:44:06 +0100

    Update bg_remove.py

--- bg_remove.py
+++ bg_remove.py
@@ -8,6 +8,7 @@ st.set_page_config(layout="wide", page_title="Image Background Remover")
 
 e = RuntimeError('This is an exception of type RuntimeError')
 st.exception(e)
+st.write("## Test")
 st.write("## Remove background from your image")
 st.write(
     ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
